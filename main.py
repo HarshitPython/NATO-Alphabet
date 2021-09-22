a95449388ee4cf9ab242fce93e66c1b0bef8c98b
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-09-22 11:35:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"], 
-#     "score": [56, 76, 98]
-# }
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
 
 # #Looping through dictionaries:
 # for (key, value) in student_dict.items():
