e23a10b7a8861fa56ec36eb7f35a22ad81083431
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-09-24 12:42:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 :
 
-# # Keyword Method with iterrows()
+# Keyword Method with iterrows()
 # # {new_key:new_value for (index, row) in df.iterrows()}
 
 # #TODO 1. Create a dictionary in this format:
