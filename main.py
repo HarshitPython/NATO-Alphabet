123efa61af0f947f45e88c7b03a573a778ed75d2
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-09-25 12:41:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ a = pd.read_csv("alphabet.csv")
 phonetic_dict = {row.letter:row.code for (index,row) in a.iterrows()}
 # print(phonetic_dict)
 
-# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 # c = input("Enter a word: ").upper()
 # b = [phonetic_dict[letter] for letter in c]
