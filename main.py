d5847d3d33ca479ccf6edb99f2adc356c661af8d
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-09-25 12:41:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ phonetic_dict = {row.letter:row.code for (index,row) in a.iterrows()}
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 c = input("Enter a word: ").upper()
-# b = [phonetic_dict[letter] for letter in c]
+b = [phonetic_dict[letter] for letter in c]
 # print(b)
